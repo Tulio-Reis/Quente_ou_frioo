f08d920ab4769c0bbd2701d7bfad8f0c9532050c
Túlio-Reis <tuliovasconce@gmail.com>
2026-01-17 14:35:36 -0300

    Atualizado

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Random;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        int numeroSecreto = new Random().nextInt(101);
+        Scanner tentativa = new Scanner(System.in);
+        System.out.println("O número secreto é:" + numeroSecreto);
+        System.out.println("Olá, guardo um número MUITO poderoso em minha memória... \nUm número ao qual, aquele que tem acesso, detém o poder.\n");
+        System.out.println("Acerte o número especial (0-100), e descubra o verdadeiro poder!\n");
+        System.out.println(" *** Vamos lá forasteiro, qual seu paplpite? ***");
+
+        boolean acertou = false;
+//System.out.println(numeroSecreto);
+
+        for (int i = 0; i < 7; i++) {
+            int palpite = tentativa.nextInt();
+
+
+            if (palpite == numeroSecreto) {
+                System.out.println("TRRRRRRRRRRUMMMMMMMMM, A TERRA TREME COM A DESCOBERTA DO NÚMERO DO PODER!!!");
+                System.out.print(" Comemore, os céus agitam por sua sabedoria!\n Desfrute desde poder IMENSURÁVEL!! ");
+
+                acertou = true;
+
+                break;
+            } else {
+                int chance = 6 - i;
+                if (chance > 0) {
+                    String palavra;
+                    if (chance == 1) {
+                        palavra = "chance";
+                    } else
+                        palavra = "chances";
+
+                    if (palpite > numeroSecreto + 10 || palpite < numeroSecreto - 10)
+                        System.out.println("Seu palpite está frio... A diferença entre seu chute e o número do poder é maior que 10... \nVocê ainda possui " + chance + " " + palavra);
+
+                    else {
+                        System.out.println("Seu palpite está quente... A diferença entre seu chute e o número do poder é menor que 10... \nVocê ainda possui " + chance + " " + palavra);
+                    }
+
+                }
+                if (chance == 0) {
+                    System.out.println("Você falhou, suas chances terminaram, Diga adeus ao verdadeiro poder! \nO número do poder era " + numeroSecreto);
+                }
+
+            }
+
+        }
+    }
 }
+
+
+
